b4b46183352f13759c238f67a783236eadcd3a98
busrasariguzel <busra.sariguzel@codeimmersives.com>
2020-03-16 15:12:32 -0400

    created login and register routes

--- routes/users/userRoutes.js
+++ routes/users/userRoutes.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('main/welcome', { title: 'Express' });
-});
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('main/welcome', { title: 'Express' });
+// });
+
+
+router.get('/', (req,res)=>{
+  return res.render('main/welcome')
+})
+
+router.get('/login', (req,res)=>{
+  return res.render('main/login')
+})
+router.get('/register', (req,res)=>{
+  return res.render('main/register')
+})
 
 module.exports = router;

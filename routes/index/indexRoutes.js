7726a360455a1efaa443f1147a6b74d300312a08
busrasariguzel <busra.sariguzel@codeimmersives.com>
2020-03-18 15:38:45 -0400

    created head partial

--- routes/index/indexRoutes.js
+++ routes/index/indexRoutes.js
@@ -2,16 +2,18 @@ const express = require('express');
 const router = express.Router();
 require('../../lib/passport')
 
-
-router.get('/about', (req,res)=>{
-    return res.render('main/about')
-});
-
 router.get('/', function(req, res, next) {
     return res.render('index', { title: 'Express' });
 });
 
 
+router.get('/about', (req,res)=>{
+    return res.render('main/about')
+});
+
+router.get('/home', (req,res)=>{
+    return res.render('main/home')
+});
 
 
 module.exports= router
